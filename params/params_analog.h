bc71cb918c110d445d491687994226971d07e9bb
artkeller <artkeller@gmx.de>
2025-10-14 05:24:18 +0200

    Update params_analog.h

--- params/params_analog.h
+++ params/params_analog.h
@@ -21,34 +21,33 @@
     {filter_type, length, coeffs, coeffs_len, freq_hz, decay_ms, warmup_ms, threshold, 0.0f, VALUE_MODE _TRAILER} \
   }
 
-/* TO be adapted
 // Vordefinierte Filter für Szenarien
-const std::vector<FilterConfig> filter_analog_brum = {
-  {FIR, 5, notch_50hz, 5, 100.0f, 10000, 200, 5.0f, 0.0f, VALUE_MODE} // 50 Hz Notch
+const std::vector<FilterConfig> filter_analog_notch = {
+//  {FIR, 5, notch_50hz, 5, 100.0f, 10000, 200, 5.0f, 0.0f, VALUE_MODE _TRAILER} // 50 Hz Notch
 };
 
 const std::vector<FilterConfig> filter_analog_vdiv = {
-  {EMA, 10, nullptr, 0, 10.0f, 3600000, 1000, 1.0f, 0.0f, VALUE_MODE}
+  {EMA, 10, nullptr, 0, 10.0f, 3600000, 1000, 1.0f, 0.0f, VALUE_MODE _TRAILER}
 };
 
 const std::vector<FilterConfig> filter_analog_poti = {
-  {EMA, 10, nullptr, 0, 10.0f, 3600000, 1000, 2.0f, 0.0f, VALUE_MODE}
+  {EMA, 10, nullptr, 0, 10.0f, 3600000, 1000, 2.0f, 0.0f, VALUE_MODE _TRAILER}
 };
 
 const std::vector<FilterConfig> filter_analog_sensor = {
-  {SMA, 10, nullptr, 0, 1.0f, 86400000, 2000, 2.0f, 0.0f, VALUE_MODE}
+  {SMA, 10, nullptr, 0, 1.0f, 86400000, 2000, 2.0f, 0.0f, VALUE_MODE _TRAILER}
 };
 
 const std::vector<FilterConfig> filter_analog_transient = {
-  {FIR, 5, notch_50hz, 5, 100.0f, 1000, 100, 5.0f, 0.0f, VALUE_MODE}
+  {FIR, 5, notch_50hz, 5, 100.0f, 1000, 100, 5.0f, 0.0f, VALUE_MODE _TRAILER}
 };
 
 const std::vector<FilterConfig> filter_analog_drift = {
-  {EMA, 20, nullptr, 0, 0.1f, 86400000, 10000, 1.0f, 0.0f, VALUE_MODE}
+  {EMA, 20, nullptr, 0, 0.1f, 86400000, 10000, 1.0f, 0.0f, VALUE_MODE _TRAILER}
 };
-*/
 
 #endif
+
 
 
 
